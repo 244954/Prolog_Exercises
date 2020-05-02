fe66d659016c5717c822de9345ffbb382f23afd0
244954 <mateusz_m_malecki@interia.pl>
2020-05-02 23:39:00 +0200

    Zadania 2 i 3 z listy 6

--- lista6/zadanie2.pl
+++ lista6/zadanie2.pl
@@ -1,4 +1,100 @@
-﻿biały(' ' ).
+﻿wykonaj(NazwaPliku) :-
+	open(NazwaPliku, read, X),
+	scanner(X, Y),
+	close(X),
+	phrase(program(PROGRAM), Y),
+	interpreter(PROGRAM, []).
+
+podstaw([], ID, N, [ID = N]).
+podstaw([ID=_ | AS], ID, N, [ID=N | AS]) :- !.
+podstaw([ID1=W1 | AS1], ID, N, [ID1=W1 | AS2]) :-
+    podstaw(AS1, ID, N, AS2).
+
+pobierz([ID=N | _], ID, N) :- !.
+pobierz([_ | AS], ID, N) :-
+    pobierz(AS, ID, N).
+
+wartość(int(N), _, N).
+wartość(id(ID), AS, N) :-
+    pobierz(AS, ID, N).
+wartość(W1 + W2, AS, N) :-
+    wartość(W1, AS, N1),
+    wartość(W2, AS, N2),
+    N is N1 + N2.
+wartość(W1 - W2, AS, N) :-
+    wartość(W1, AS, N1),
+    wartość(W2, AS, N2),
+    N is N1 - N2.
+wartość(W1 * W2, AS, N) :-
+    wartość(W1, AS, N1),
+    wartość(W2, AS, N2),
+    N is N1 * N2.
+wartość(W1 / W2, AS, N) :-
+    wartość(W1, AS, N1),
+    wartość(W2, AS, N2),
+    N2 =\= 0,
+    N is N1 div N2.
+wartość(W1 mod W2, AS, N) :-
+    wartość(W1, AS, N1),
+    wartość(W2, AS, N2),
+    N2 =\= 0,
+    N is N1 mod N2.
+
+prawda(W1 =:= W2, AS) :-
+    wartość(W1, AS, N1), wartość(W2, AS, N2),
+    N1 =:= N2.
+prawda(W1 =\= W2, AS) :-
+    wartość(W1, AS, N1), wartość(W2, AS, N2),
+    N1 =\= N2.
+prawda(W1 < W2, AS) :-
+    wartość(W1, AS, N1), wartość(W2, AS, N2),
+    N1 < N2.
+prawda(W1 > W2, AS) :-
+    wartość(W1, AS, N1), wartość(W2, AS, N2),
+    N1 > N2.
+prawda(W1 >= W2, AS) :-
+    wartość(W1, AS, N1), wartość(W2, AS, N2),
+    N1 >= N2.
+prawda(W1 =< W2, AS) :-
+    wartość(W1, AS, N1), wartość(W2, AS, N2),
+    N1 =< N2.
+prawda((W1, W2), AS) :-
+    prawda(W1, AS),
+    prawda(W2, AS).
+prawda((W1; W2), AS) :-
+    (   prawda(W1, AS),
+        !
+    ;   prawda(W2, AS)).
+
+interpreter([], _).
+interpreter([read(ID) | PGM], ASSOC) :- !,
+    read(N),
+    integer(N),
+    podstaw(ASSOC, ID, N, ASSOC1),
+    interpreter(PGM, ASSOC1).
+interpreter([write(W) | PGM], ASSOC) :- !,
+    wartość(W, ASSOC, WART),
+    write(WART), nl,
+    interpreter(PGM, ASSOC).
+interpreter([assign(ID,W) | PGM], ASSOC) :- !,
+    wartość(W, ASSOC, WAR),
+    podstaw(ASSOC, ID, WAR, ASSOC1),
+    interpreter(PGM, ASSOC1).
+interpreter([if(C,P) | PGM], ASSOC) :- !,
+    interpreter([if(C, P, []) | PGM], ASSOC).
+interpreter([if(C,P1,P2) | PGM], ASSOC) :- !,
+    (   prawda(C, ASSOC)
+    ->  append(P1, PGM, DALEJ)
+    ;   append(P2, PGM, DALEJ)),
+    interpreter(DALEJ, ASSOC).
+interpreter([while(C,P) | PGM], ASSOC) :- !,
+    append(P, [while(C, P)], DALEJ),
+    interpreter([if(C, DALEJ) | PGM], ASSOC).
+
+%%%%%%%%%%%%%%%%%%%%%%
+% parser i skaner
+
+biały(' ' ).
 biały('\t').
 biały('\n').
 znak('+').
@@ -98,7 +194,6 @@ zmień_słowo(Upper,id(Upper)) :-
     X == Upper,
     !.
    % get_string_code(1,Upper,First),code_type(First,upper),!
-
 zmień_słowo(H,H) :- write('Nielegalne słowo: '), write(H),nl.
 
 
@@ -124,14 +219,6 @@ wyrażenie(id(ID) / WYRAŻENIE2) --> [id(ID)], [sep(/)], wyrażenie(WYRAŻENIE2)
 wyrażenie(int(NUM) / WYRAŻENIE2) --> [int(NUM)], [sep(/)], wyrażenie(WYRAŻENIE2).
 wyrażenie(id(ID) mod WYRAŻENIE2) --> [id(ID)], [key(mod)], wyrażenie(WYRAŻENIE2).
 wyrażenie(int(NUM) mod WYRAŻENIE2) --> [int(NUM)], [key(mod)], wyrażenie(WYRAŻENIE2).
-% wyrażenie(WYRAŻENIE1 - WYRAŻENIE2) --> wyrażenie(WYRAŻENIE1), [sep(-)],
-% wyrażenie(WYRAŻENIE2).
-% wyrażenie(WYRAŻENIE1 * WYRAŻENIE2) --> wyrażenie(WYRAŻENIE1), [sep(*)],
-% wyrażenie(WYRAŻENIE2).
-% wyrażenie(WYRAŻENIE1 / WYRAŻENIE2) --> wyrażenie(WYRAŻENIE1), [sep(/)],
-% wyrażenie(WYRAŻENIE2).
-% wyrażenie(WYRAŻENIE1 mod WYRAŻENIE2) --> wyrażenie(WYRAŻENIE1),
-% [key(mod)], wyrażenie(WYRAŻENIE2).
 
 warunek(WYRAŻENIE1 =:= WYRAŻENIE2) --> wyrażenie(WYRAŻENIE1), [sep(=)], wyrażenie(WYRAŻENIE2).
 warunek(WYRAŻENIE1 =\= WYRAŻENIE2) --> wyrażenie(WYRAŻENIE1), [sep(/=)], wyrażenie(WYRAŻENIE2).
@@ -153,9 +240,3 @@ warunek(WYRAŻENIE1 < WYRAŻENIE2 ',' WARUNEK2) --> wyrażenie(WYRAŻENIE1), [se
 warunek(WYRAŻENIE1 > WYRAŻENIE2 ',' WARUNEK2) --> wyrażenie(WYRAŻENIE1), [sep(>)], wyrażenie(WYRAŻENIE2), [key(and)], warunek(WARUNEK2).
 warunek(WYRAŻENIE1 =< WYRAŻENIE2 ',' WARUNEK2) --> wyrażenie(WYRAŻENIE1), [sep(=<)], wyrażenie(WYRAŻENIE2), [key(and)], warunek(WARUNEK2).
 warunek(WYRAŻENIE1 >= WYRAŻENIE2 ',' WARUNEK2) --> wyrażenie(WYRAŻENIE1), [sep(>=)], wyrażenie(WYRAŻENIE2), [key(and)], warunek(WARUNEK2).
-
-
-
-% warunek(WARUNEK1 ; WARUNEK2) --> warunek(WARUNEK1), [key(or)],
-% warunek(WARUNEK2). warunek(WARUNEK1 ',' WARUNEK2) -->
-% warunek(WARUNEK1), [key(and)], warunek(WARUNEK2).
---
+++ lista6/zadanie3.pl
@@ -0,0 +1,24 @@
+﻿gramatyka1 --> ab.
+ab --> ``.
+ab --> `a`,ab,`b`.
+
+gramatyka2 --> ac(0).
+ac(X) --> `a`,{Y is X + 1},ac(Y),`c`.
+ac(X) --> b(X).
+b(0) --> ``.
+b(X) --> `b`,{Y is X - 1}, b(Y).
+
+gramatyka3 --> a(0).
+a(X) --> `a`,{Y is X + 1},a(Y).
+a(X) --> {fib(X,Y)}, b(Y).
+
+fib(0,1) :-
+    !.
+fib(1,1) :-
+    !.
+fib(N,M) :-
+    N1 is N - 1,
+    N2 is N - 2,
+    fib(N1,M1),
+    fib(N2,M2),
+    M is M1 + M2.
